b98aa710e9d50fdd7cf28f24c1d7289f650b48a1
Darrell10 <frederick.port@me.com>
2019-08-01 13:54:51 +0200

    Initial Commit

---
+++ Projet 3 : RPG/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projet 3 : RPG
+//
+//  Created by Frederick Port on 01/08/2019.
+//  Copyright © 2019 French game factory. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
